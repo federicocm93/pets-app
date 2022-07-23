b146a0b5897470464c2fe016fa8b92a4d272bf41
Federico <fcmontagner@gmail.com>
2022-07-22 22:33:47 -0300

    Add PetCard component. Add Infinite scroll library.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { Link, Outlet } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Button variant="contained">
-        <Link to="/list">Found list</Link>
-      </Button>
       <Outlet />
     </div>
   );
--- src/components/FoundList.jsx
+++ src/components/FoundList.jsx
@@ -1,28 +1,71 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { getFoundList } from '../data';
+import PetCard from "./PetCard";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useEffect, useState } from "react";
+import Container from '@mui/material/Container';
 
 export default function FoundList() {
-  let foundList = getFoundList();
+  const [foundList, setFoundList] = useState([]);
+
+  useEffect(() => {
+    setFoundList(getFoundList());
+  }, []);
+
+  // TODO: Replace with api call
+  const getMoreFound = () => {
+    setTimeout(() => {
+      setFoundList(foundList.concat([
+        {
+          id: foundList.length + 1,
+          name: "Negro",
+          breed: "Golden retriever",
+          image: "https://t1.ea.ltmcdn.com/es/posts/1/6/2/10_curiosidades_del_golden_retriever_21261_600.jpg",
+          when: "12/05/2022"
+        },
+        {
+          id: foundList.length + 2,
+          name: "Mora",
+          breed: "Beagle",
+          image: "https://okdiario.com/img/2022/05/10/beagle.jpg",
+          when: "22/06/2022"
+        },
+        {
+          id: foundList.length + 3,
+          name: "Tito",
+          breed: "Pitbull",
+          image: "https://mascotafiel.com/wp-content/uploads/2014/03/pitbull-portada.jpg",
+          when: "06/07/2022"
+        },
+        {
+          id: foundList.length + 4,
+          name: "Lala",
+          breed: "Dogo argentino",
+          image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2gez11MzStYNVGInwB3gI1-Q7rOH6hkV2rA&usqp=CAU",
+          when: "27/04/2022"
+        },
+      ]));
+    }, 1500);
+  };
 
   return (
-    <div style={{ display: "flex" }}>
-      <nav
-        style={{
-          borderRight: "solid 1px",
-          padding: "1rem",
-        }}
+    <Container maxWidth="sm">
+      <InfiniteScroll
+        dataLength={foundList.length}
+        next={getMoreFound}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
       >
         {foundList.map((pet) => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
-            to={`/list/${pet.id}`}
-            key={pet.id}
-          >
-            {pet.name}
-          </Link>
-        ))}
-      </nav>
+        <PetCard
+          key={pet.id}
+          name={pet.name}
+          breed={pet.breed}
+          image={pet.image}
+        />
+      ))}
+      </InfiniteScroll>
       <Outlet />
-    </div>
+    </Container>
   );
 }
