cd7faa2382799de417136ef5ac23c5ab3433a654
Federico <fcmontagner@gmail.com>
2022-08-15 17:00:21 -0300

    Re-design sidebar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
 import { Outlet } from "react-router-dom";
-import Sidebar from "./components/sidebar/Sidebar";
+import PersistentSidebar from "./components/persistent-sidebar/PersistentSidebar";
+import Pets from '@mui/icons-material/Pets';
+import EmojiPeople from '@mui/icons-material/EmojiPeople';
 
 function App() {
+
+  const sidebarMainOptions =  [
+    {
+      text: "Perdidos",
+      Icon: Pets,
+      path: "/list"
+    },
+  ];
+
+  const sidebarSecondaryOptions = [
+    {
+      text: "Nosotros",
+      Icon: EmojiPeople,
+      path: "/about"
+    },
+  ];
+
   return (
-    <div>
-      <Sidebar />
-      <Outlet />
-    </div>
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item md={4}>
+          <PersistentSidebar
+            title={"Pets App"}
+            mainOptions={sidebarMainOptions}
+            secondaryOptions={sidebarSecondaryOptions}/>
+        </Grid>
+        <Grid item md={8}>
+          <Outlet />
+        </Grid>
+      </Grid>
+    </Container>
   );
 
 }
---
+++ src/components/persistent-sidebar/PersistentSidebar.jsx
@@ -0,0 +1,139 @@
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { Link } from "react-router-dom";
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function PersistentSidebar({title, mainOptions, secondaryOptions}) {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {mainOptions.map(({text, Icon, path}, index) => (
+            <Link to={path} style={{ textDecoration: 'none', color: 'black' }}>
+              <ListItem key={index} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Icon />
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {secondaryOptions.map(({text, Icon, path}, index) => (
+            <Link
+              to={path}
+              // Had to use inline styles because they do not work inside links
+              style={{
+                textDecoration: 'none',
+                color: 'black',
+                position: "fixed",
+                bottom: 0,
+                textAlign: "center",
+                paddingBottom: 10
+              }}>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Icon />
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+            </Link>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+}
